a0c7eddb3f6f3c9a5fcc4f0b4f469bb14a5dcabf
zhoujia <zhoujia@meituan.com>
2017-06-21 11:55:58 +0800

    remove private info

--- client/src/test/Main.java
+++ client/src/test/Main.java
@@ -13,7 +13,10 @@ import yunbi.YunBi;
  * Created by zhoujia on 2017/6/20.
  */
 public class Main {
-
+    /**
+     * 使用前在yunbi.Config中注册access_key和secret_key
+     * @param args
+     */
     public static void main(String[] args) {
         CoinInfo coinInfo = new CoinInfo("cn", "yunbi", "sc", "yunbi_sc", Dimension.MIN_1.getType());
         Coin.getInstance().registerStrategy(coinInfo, new StrategyDealWrapper(new SmartGDFStrategy("sc模拟" + Dimension.MIN_1.getName(), 0.002), new SmartGDFTrade("sccny")));
--- client/src/yunbi/Config.java
+++ client/src/yunbi/Config.java
@@ -4,6 +4,6 @@ package yunbi;
  * Created by zhoujia on 2017/6/16.
  */
 public class Config {
-    public static final String SERCET_KEY = "nHXp2R8aq4kxZxa5Bs1uk1uKB0TNABvjfhEnZOFK";
-    public static final String ACCESS_KEY = "aW6gCXN6J8NTjUx3MZz81vOzV8x8M4eupNYHD5Bd";
+    public static final String SERCET_KEY = "";
+    public static final String ACCESS_KEY = "";
 }
